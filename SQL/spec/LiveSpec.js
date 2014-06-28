37b2a5dbd5d620f540eb2e002ce5effc81066a89
Mason <maseh87@gmail.com>
2014-06-27 18:49:05 -0700

    deleted files I wont need

--- SQL/spec/LiveSpec.js
+++ SQL/spec/LiveSpec.js
@@ -11,9 +11,9 @@ describe("Persistent Node Chat Server", function() {
   beforeEach(function(done) {
     dbConnection = mysql.createConnection({
     /* TODO: Fill this out with your mysql username */
-      user: "",
+      user: "mase",
     /* and password. */
-      password: "",
+      password: "masonh87",
       database: "chat"
     });
     dbConnection.connect();
